0a0009a2f8480d067bd6965a95f583de45b92003
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-08-02 10:33:37 -0700

    finish comments on main.js

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,7 @@ async function deleteItem() {
   try {
     // set response variable with a value of what the asynchronous fetch function returns, the first argument is the resource or URL you want to fetch from
     const response = await fetch('deleteItem', {
-      // HTTP request type of delete
+      // HTTP request type of DELETE
       method: 'delete',
       //   Saying that we are sending content as JSON
       headers: { 'Content-Type': 'application/json' },
@@ -54,38 +54,64 @@ async function deleteItem() {
   }
 }
 
+// Define a asynchronous markComplete function with no parameters
 async function markComplete() {
+  // create variable with a value of the inner text from a sibling element to the element that was clicked on
   const itemText = this.parentNode.childNodes[1].innerText;
+  //   start try catch block (try to do some code and if it throws errors catch them)
   try {
+    // set response variable with a value of what the asynchronous fetch function returns, the first argument is the resource or URL you want to fetch from
     const response = await fetch('markComplete', {
+      // HTTP request type of PUT
       method: 'put',
+      //   Saying that we are sending content as JSON
       headers: { 'Content-Type': 'application/json' },
+      //   Setting the body and converting it to JSON
       body: JSON.stringify({
+        // key value pair and setting itemFromJS with a value of the itemText from the html document
         itemFromJS: itemText,
       }),
     });
+    // Setting a variable data equal to the response converted to JSON
     const data = await response.json();
+    // Log the data to the console in the browser
     console.log(data);
+    // Refresh the current page (this will allow the deleted item to disappear)
     location.reload();
+    // Catch errors if the try block isn't successful
   } catch (err) {
+    // Log those errors to the browsers console.
     console.log(err);
   }
 }
 
+// Define a asynchronous markUnComplete function with no parameters
 async function markUnComplete() {
+  // create variable with a value of the inner text from a sibling element to the element that was clicked on
   const itemText = this.parentNode.childNodes[1].innerText;
+  //   start try catch block (try to do some code and if it throws errors catch them)
   try {
+    // set response variable with a value of what the asynchronous fetch function returns, the first argument is the resource or URL you want to fetch from
     const response = await fetch('markUnComplete', {
+      // HTTP request type of PUT
       method: 'put',
+      //   Saying that we are sending content as JSON
       headers: { 'Content-Type': 'application/json' },
+      //   Setting the body and converting it to JSON
       body: JSON.stringify({
+        // key value pair and setting itemFromJS with a value of the itemText from the html document
         itemFromJS: itemText,
       }),
     });
+    // Setting a variable data equal to the response converted to JSON
     const data = await response.json();
+    // Log the data to the console in the browser
     console.log(data);
+    // Refresh the current page (this will allow the deleted item to disappear)
     location.reload();
+    // Catch errors if the try block isn't successful
   } catch (err) {
+    // Log those errors to the browsers console.
     console.log(err);
   }
 }
